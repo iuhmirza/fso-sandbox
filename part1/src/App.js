a22eb9d9db152882522d919c27f22725d1c5aaa9
ibtisam <iuhmirza@gmail.com>
2022-09-01 18:12:34 +0100

    Refactor function components

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,21 +1,24 @@
 import {useState} from 'react'
 
+const Display = ({counter}) => <div>{counter}</div>
+
+const Button = ({onClick, text}) => <button onClick={onClick}>{text}</button>
+
 const App = () => {
   const [counter, setCounter] = useState(0)
 
   const increment = () => setCounter(counter + 1)
 
+  const decrement = () => setCounter(counter - 1)
+
   const reset = () => setCounter(0)
 
   return (
     <div>
-      <div>{counter}</div>
-      <button onClick={increment}>
-        increment
-      </button>
-      <button onClick={reset}>
-        reset
-      </button>
+      <Display counter={counter} />
+      <Button onClick = {increment} text='increment' />
+      <Button onClick = {reset} text = 'reset' />
+      <Button onClick = {decrement} text = 'decrement' />
     </div>
   )
 }
