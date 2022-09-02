89d2905bbfb696fc40a3a36b086e22b32a3d9a78
Ibtisam <81363048+iuhmirza@users.noreply.github.com>
2022-09-02 11:53:03 +0100

    Handle state in single object

--- part1d/src/App.js
+++ part1d/src/App.js
@@ -2,17 +2,34 @@ import {useState} from 'react'
 
 const App = () => {
 
-    const [left, setLeft] = useState(0)
-    const [right, setRight] = useState(0)
+    const [clicks, setClicks] = useState({
+        left: 0, right: 0
+    })
+
+    const handleLeftClick = () => {
+        const newClicks = {
+            left: clicks.left + 1,
+            right: clicks.right
+        }
+        setClicks(newClicks)
+    }
+
+    const handleRightClick = () => {
+        const newClicks = {
+            left: clicks.left,
+            right: clicks.right + 1
+        }
+        setClicks(newClicks)
+    }
 
     return (
         <div>
-            {left}
-            <button onClick={() => setLeft(left+1)}>
+            {clicks.left}
+            <button onClick={handleLeftClick}>
                 left
             </button>
-            {right}
-            <button onClick={() => setRight(right+1)}>
+            {clicks.right}
+            <button onClick={handleRightClick}>
                 right
             </button>
         </div>
