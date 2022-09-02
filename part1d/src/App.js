d6cc830f65db2451df2d6ae2436d17d96d7fa7c2
Ibtisam <81363048+iuhmirza@users.noreply.github.com>
2022-09-02 11:48:17 +0100

    Add buttons with state

--- part1d/src/App.js
+++ part1d/src/App.js
@@ -1,3 +1,22 @@
-const App = () => {}
+import {useState} from 'react'
+
+const App = () => {
+
+    const [left, setLeft] = useState(0)
+    const [right, setRight] = useState(0)
+
+    return (
+        <div>
+            {left}
+            <button onClick={() => setLeft(left+1)}>
+                left
+            </button>
+            {right}
+            <button onClick={() => setRight(right+1)}>
+                right
+            </button>
+        </div>
+    )
+}
 
 export default App
