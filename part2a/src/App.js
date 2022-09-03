546d38c00b93ce811fd5b00014abc3b11a64c4e7
Ibtisam <81363048+iuhmirza@users.noreply.github.com>
2022-09-03 16:36:35 +0100

    Add node component into separate file

--- part2a/src/App.js
+++ part2a/src/App.js
@@ -1,9 +1,11 @@
+import Note from './components/Note'
+
 const App = ({notes}) => {
   return (
     <div>
       <h1>notes</h1>
       <ul>
-        {notes.map(note => <li key={note.id}>{note.content}</li>)}
+        {notes.map(note => <Note key={note.id} note={note} />)}
       </ul>
     </div>
   )
