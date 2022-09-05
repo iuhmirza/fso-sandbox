3a5be015c074d6fe7175713596249a3b21f4a57d
ibtisam <iuhmirza@gmail.com>
2022-09-05 10:15:03 +0100

    Add form

--- part2a/src/App.js
+++ part2a/src/App.js
@@ -4,12 +4,21 @@ import Note from './components/Note'
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes)
 
+  const addNote = (event) => {
+    event.preventDefault()
+    console.log('button clicked', event.target);
+  }
+
   return (
     <div>
       <h1>notes</h1>
       <ul>
         {notes.map(note => <Note key={note.id} note={note} />)}
       </ul>
+      <form onSubmit={addNote}>
+        <input />
+        <button type="submit">save</button>
+      </form>
     </div>
   )
 
