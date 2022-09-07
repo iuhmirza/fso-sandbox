2ef4a95624af82fd997870f2c2242872257d9e20
ibtisam <iuhmirza@gmail.com>
2022-09-07 12:39:36 +0100

    Add custom error message

--- part2a/src/App.js
+++ part2a/src/App.js
@@ -2,11 +2,23 @@ import {useState, useEffect} from 'react'
 import Note from './components/Note'
 import noteService from './services/notes'
 
+const Notification = ({ message }) => {
+  if (message === null) {
+    return null
+  }
+
+  return (
+    <div className='error'>
+      {message}
+    </div>
+  )
+}
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('MISSINGNO.')
 
   useEffect(() => {
     noteService
@@ -46,9 +58,12 @@ const App = () => {
       setNotes(notes.map(n => n.id !== id ? n : returnedNote))
     })
     .catch(error => {
-      alert(
-        `the note '${note.content}' was already deleted from server`
+      setErrorMessage(
+        `Note '${note.content}' was already removed from server`
       )
+      setTimeout(() => {
+        setErrorMessage(null)
+      }, 5000)
       setNotes(notes.filter(n => n.id !== id))
     })
   }
@@ -60,6 +75,7 @@ const App = () => {
   return (
     <div>
       <h1>notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
