f3938bd8f5bea08717ce37b16b455c5e1ae8d82e
Ibtisam Mirza <iuhmirza@gmail.com>
2022-09-09 15:59:42 +0100

    Add environment port

--- part3/index.js
+++ part3/index.js
@@ -81,6 +81,7 @@ app.post('/api/notes', (request, response) => {
   response.json(note)
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
